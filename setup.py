d0a66a95e80f12d837800a13323f6d19e5a5800c
Felipe Orellana <forellana-digithinkit@users.noreply.github.com>
2019-06-19 13:10:11 -0400

    fix: Frappe fails to install with pip newer than v9.0.3
    This adds an alternative parse_requirements method removed on pip >= v10

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ import os, shutil
 from distutils.command.clean import clean as Clean
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip < 10
+    from pip.req import parse_requirements
+except ImportError: # for pip >= 10
+    def parse_requirements(filename):
+        """ load requirements from a pip requirements file """
+        lineiter = (line.strip() for line in open(filename))
+        return [line for line in lineiter if line and not line.startswith("#")]
+
 import re, ast
 
 # get version from __version__ variable in frappe/__init__.py
