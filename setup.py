1929ba79c5d55a27ad83f0f3bff4bbabe83ccb85
Felipe Orellana <forellana-digithinkit@users.noreply.github.com>
2019-06-19 13:17:31 -0400

    fix: takes care of extra deprecated arguments on parse_requirements
    Quick fix on extra unused arguments to match old parse_requirements method

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 try: # for pip < 10
     from pip.req import parse_requirements
 except ImportError: # for pip >= 10
-    def parse_requirements(filename):
+    def parse_requirements(filename, **kwargs):
         """ load requirements from a pip requirements file """
         lineiter = (line.strip() for line in open(filename))
         return [line for line in lineiter if line and not line.startswith("#")]
