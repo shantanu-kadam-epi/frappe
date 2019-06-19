337d8af855251f664d51ee3622492175490d6766
Felipe Orellana <forellana-digithinkit@users.noreply.github.com>
2019-06-19 13:36:06 -0400

    fix: updated parse_requirements to return obj
    Old deprecated result contained a class that I don't want to mess with to keep things working with old and newer setup tools. So I added a stand in class to mimic the calls

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ from setuptools import setup, find_packages
 try: # for pip < 10
     from pip.req import parse_requirements
 except ImportError: # for pip >= 10
+    class ReqInfo:
+        def __init__(self, req):
+            self.req = req
+            self._link = False
+
     def parse_requirements(filename, **kwargs):
         """ load requirements from a pip requirements file """
         lineiter = (line.strip() for line in open(filename))
-        return [{ "req": line, "_link": False} for line in lineiter if line and not line.startswith("#")]
+        return [ReqInfo(line) for line in lineiter if line and not line.startswith("#")]
 
 import re, ast
 
