3a9bfbfb3bbbc83667637a32020bc1e3ed76ba1d
Himanshu <himanshuwarekar@yahoo.com>
2021-02-09 17:30:40 +0530

    Update frappe/public/js/frappe/views/communication.js

--- frappe/public/js/frappe/views/communication.js
+++ frappe/public/js/frappe/views/communication.js
@@ -139,7 +139,7 @@ frappe.views.CommunicationComposer = Class.extend({
 		this.dialog.set_value("recipients", this.recipients || '');
 		this.dialog.set_value("cc", this.cc || '');
 		this.dialog.set_value("bcc", this.bcc || '');
-		this.dialog.set_value("attach_document_print", this.attach_document_print ||this.system_settings.attach_document_print || false);
+		this.dialog.set_value("attach_document_print", this.attach_document_print || this.system_settings.attach_document_print || false);
 		this.dialog.set_value("send_me_a_copy", this.send_me_a_copy || this.system_settings.send_me_a_copy || false);
 		this.dialog.set_value("send_read_receipt", this.send_read_receipt || this.system_settings.send_read_receipt || false);
 
@@ -724,4 +724,3 @@ frappe.views.CommunicationComposer = Class.extend({
 		return text.replace(/\n{3,}/g, '\n\n');
 	}
 });
-
